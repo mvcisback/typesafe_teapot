cbc85320ee1cd14cb90f18d8252e2ed7f17bc07b
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-04-04 20:30:42 -0500

    fixed type issue in computing normals

--- Obj.hs
+++ Obj.hs
@@ -17,5 +17,7 @@ toTriangles (verts, faces) = map getCoords faces
     where getCoords (Face f1 f2 f3) = (getVert f1, getVert f2, getVert f3)
           getVert f = let (Vertex v1 v2 v3) =  verts !! f in v1:.v2:.v3:.()
 
-computeNormal (v1, v2, v3) = normalize $ (v3-v2) `cross` (v2-v1)
-computeNormals [(v1, v2, v3)] = map computeNormal
+computeNormal :: (Vec3 Float, Vec3 Float, Vec3 Float) -> Vec3 Float
+computeNormal (v1, v2, v3) = normalize $ v3-v2 `cross` v2-v1
+
+computeNormals = map computeNormal
