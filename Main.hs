80f3f60201df05805ba156c21c7a44ea69cd3c8b
Marcell Vazquez-Chanlatte <mvc@linux.com>
2014-04-14 12:35:54 -0500

    actually use lighting flag

--- Main.hs
+++ Main.hs
@@ -9,8 +9,8 @@ import Data.IORef
 import Control.Applicative
 import Control.Monad
 import Obj
-import System.IO    
-import System.Exit    
+import System.IO
+import System.Exit
 import Graphics.UI.GLUT
     (mainLoop,
     postRedisplay,
@@ -49,23 +49,24 @@ main = execParser opts >>= render
                <> progDesc ""
                <> header "")
 
-render (Opts texs envs bumps lighting)= do
+render (Opts texs envs bumps lightingFlag)= do
   getArgsAndInitialize
   tex <- loadTexture RGB8 texs :: IO (Texture2D RGBFormat)
   env <- loadTexture RGB8 envs :: IO (Texture2D RGBFormat)
   bumps <- loadTexture RGB8 bumps :: IO (Texture2D RGBFormat)
   angleRef <- newIORef 0.0
   obj <- getContents >>= (getObj . objToGPU)
-  newWindow "Spinning box" (100:.100:.()) (800:.600:.()) 
-       (renderFrame (tex,env,bumps) angleRef obj)
+  newWindow "Spinning box" (100:.100:.()) (800:.600:.())
+       (renderFrame (tex,env,bumps) lighting angleRef obj)
        initWindow
   mainLoop
       where getObj (Left obj) = return $ toGPUStream TriangleList obj
             getObj (Right s) = print s >> exitFailure
+            lighting = if lightingFlag then seeliger else phong
 
-renderFrame texs angleRef obj size = readIORef angleRef >>= nextFrame
+renderFrame texs lighting angleRef obj size = readIORef angleRef >>= nextFrame
     where nextFrame angle = writeIORef angleRef ((angle + 0.005) `mod'` (2*pi))
-                            >> return (objFrameBuffer (enlight seeliger) texs angle size obj)
+                            >> return (objFrameBuffer (enlight lighting) texs angle size obj)
 initWindow win = idleCallback $= Just (postRedisplay (Just win))
 
 type TriangleStream = PrimitiveStream Triangle (Vec3 (Vertex Float), Vec3 (Vertex Float), Vec2 (Vertex Float))
@@ -82,7 +83,7 @@ light = toGPU $ normalize (1:.1:.1:.())
 view = toGPU 0:.0:.1:.()
 
 phong norm (texColor,envColor) = RGB $ (texColor+envColor)*Vec.vec (specular + ambient + diffuse)
-    where diffuse = maxB (norm `dot` light) 0 
+    where diffuse = maxB (norm `dot` light) 0
           ambient = toGPU 0.1
           specular = ifB (proj >* 0) (abs $ view `dot` r ** n) 0
               where r = Vec.vec (2* proj) * norm - light
@@ -93,11 +94,11 @@ seeliger norm (color,_) = RGB $ color * Vec.vec (ifB (s ==* 0 &&* t ==* 0) 0 (s 
     where s = maxB (norm `dot` light) 0
           t = maxB (norm `dot` view) 0
 
-bumpedNormal bumps norm@(nx:.ny:.nz:.()) uv@(u:.v:.()) = 
+bumpedNormal bumps norm@(nx:.ny:.nz:.()) uv@(u:.v:.()) =
     normalize $ norm - (dBdu * norm) `cross` dSdv + (dBdv * norm) `cross` dSdu
-    where 
+    where
           bumpColor = sample (Sampler Linear Wrap) bumps
-          diffS dFda = dFda nx :. dFda ny :. dFda nz :.() 
+          diffS dFda = dFda nx :. dFda ny :. dFda nz :.()
           diffB u2 v2 =  (b2 - b1) * Vec.vec (1/h)
               where RGB b1 = bumpColor $ uv
                     RGB b2 = bumpColor $ u2:.v2:.()
